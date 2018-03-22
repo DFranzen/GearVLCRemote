f07fcb09efe7479e5c76f1ba2e401392991d1ef0
dfranzen <dfranzen@github.com>
2018-03-22 15:35:12 +0200

    Restructure Code, add new features + added List view of previously added servers + added Messages for connection connection errors + added contiouse updates of remote view * Implemented the view pattern, restuctured every file into object

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 (function () {
 	window.addEventListener("tizenhwkey", function (ev) {
-		var activePopup = null,
-			page = null,
-			pageid = "";
-
-		/*if (ev.keyName === "back") {
-			activePopup = document.querySelector(".ui-popup-active");
-			page = document.getElementsByClassName("ui-page-active")[0];
-			pageid = page ? page.id : "";
-
-			if (pageid === "main" && !activePopup) {
-				try {
-					//tizen.application.getCurrentApplication().exit();
-				} catch (ignore) {
-				}
-			} else {
-				window.history.back();
-			}
-		}*/
+		console.log("Received hardwarekey: " + ev);
 	});
 }());
 
-var setPosition; //From circularToolBar.js
+var toolbar; //From circularToolBar.js
 var vlc; //From vlc.js
+
+/* TODO:
+ * input form for Name and port
+ * default values for input elements
+ * Deactivate during makeRequest
+ * Indicatoren für online servers
+ * scan network
+ */
+
+/* Git IP adress:
+ * function successCallback(wifi) {
+    	console.log("Status: " + wifi.status +  "    SSID: " + wifi.ssid
+        + "\nIP Address: " + wifi.ipAddress  + "    Signal Strength: " + wifi.signalStrength);
+    }
+ * function errorCallback(error) {
+        console.log("Wifi Not supported: " + error.message);
+    }
+    tizen.systeminfo.getPropertyValue("WIFI_NETWORK", successCallback, errorCallback);
+ */
 
 var app = {
 	back: function () {},
-		
+	rotate: function () {},
+	scroll: function(e) {
+		var scroller=document.getElementsByClassName("ui-scroller")[0];
+	    if (e.detail.direction === "CW") {
+	    	scroller.scrollTop+=50;
+	    } else if (e.detail.direction === "CCW") {
+	    	scroller.scrollTop-=50;
+	    }
+	},	
     updateView: function () {
     	document.getElementById("serverName").innerHTML = vlc.serverName;
     	document.getElementById("serverIP").innerHTML = vlc.serverIP;
@@ -56,51 +66,134 @@ var app = {
     		}
     	});
     	//Init remote View
-    	setPosition("btn1", 0);
-    	setPosition("btn2", 1);
-    	setPosition("btn3", -1);
+    	toolbar.setPosition("btn1", 0);
+    	toolbar.setPosition("btn2", 1);
+    	toolbar.setPosition("btn3", -1);
     	document.getElementById("btn3").addEventListener("click", function(){vlc.makeRequest("command=seek&val=-10S");});
     	document.getElementById("btn2").addEventListener("click", function(){vlc.makeRequest("command=seek&val=%2B10S");});
     	document.getElementById("btn1").addEventListener("click", function(){vlc.makeRequest("command=pl_pause");});
     	
+    	vlc.init();
+    	
     	//Init Server view 
-    	setPosition("btnGo", 0);
-    	document.getElementById("btnGo").addEventListener("click", function(){ app.show("remote"); });
+    	toolbar.setPosition("btnGo", 0);
+    	document.getElementById("btnGo").addEventListener("click", app.addVLCconnection);
 		
-    	app.show("server");
+    	document.addEventListener('rotarydetent', function(ev){app.rotate(ev);});
+    	
+    	app.show("serverList");
+    },
+    hideView: function (view) {
+    	var i,
+    		divs=document.getElementsByClassName(view);
+		for (i=0;i<divs.length;i++) {
+			divs[i].style.display="none";
+		}
+    },
+    hideAllViews: function() {
+    	if (app.snapList) {
+    		app.snapList.destroy();
+    		app.snapList = null;
+    	}
+    	
+    	app.hideView("viewServer");
+    	app.hideView("viewRemote");
+    	app.hideView("viewMessage");
+    	app.hideView("viewServerList");
+    },
+    showView: function (view) {
+    	var i,
+    	    divs=document.getElementsByClassName(view);
+		for (i=0;i<divs.length;i++) {
+			divs[i].style.display="block";
+		}
+    },
+    showMessage: function (message) {
+    	app.show("message");
+    	document.getElementById("messageContent").innerHTML = message;
     },
     show: function(view) {
-    	var divs,i;
     	if (view==="remote") {
     		app.view=view;
-    		divs=document.getElementsByClassName("viewRemote");
-    		for (i=0;i<divs.length;i++) {
-    			divs[i].style.display="block";
-    		}
-    		divs=document.getElementsByClassName("viewServer");
-    		for (i=0;i<divs.length;i++) {
-    			divs[i].style.display="none";
-    		}
-    		vlc.serverIP=document.getElementById("inputIP").value;
-    		vlc.password=document.getElementById("inputPassword").value;
+    		app.hideAllViews();
+    		app.showView("viewRemote");
     		
-    		localStorage.setItem("lastIP", vlc.serverIP);
-    		localStorage.setItem("lastPassword", vlc.password);
-    		
-    		vlc.getStatus();
-        	app.back=function(ev) {app.show("server"); ev.stopPropagation();};
+    		app.back=function(ev) {
+    			vlc.disconnect();
+    			app.show("serverList"); 
+    			if (ev) {
+    				ev.stopPropagation();
+    			}
+    		};
     	} else if (view==="server") {
-    		divs=document.getElementsByClassName("viewRemote");
-    		for (i=0;i<divs.length;i++) {
-    			divs[i].style.display="none";
-    		}
-    		divs=document.getElementsByClassName("viewServer");
-    		for (i=0;i<divs.length;i++) {
-    			divs[i].style.display="block";
-    		}
+    		app.hideAllViews();
+    		app.showView("viewServer");	
+    		
     		app.view=view;
-    		app.back = function() {};
+    		app.back = function(ev) {
+    			app.show("serverList"); 
+    			if (ev) {
+    				ev.stopPropagation();
+    			}
+    		};
+    	} else if (view==="serverList") {
+    		app.hideAllViews();
+    		app.showView("viewServerList");
+    		app.view=view;
+    		app.back = function() {tizen.application.getCurrentApplication().exit();};
+    		app.rotate = app.scroll;
+    		
+    		// init list:
+    		if (app.snapList) {
+    			app.snapList.destroy();
+    		}
+    		var i,li,
+    			list=document.getElementById("serverList");
+    		list.innerHTML = "";
+    		for (i=0;i<vlc.serverList.length;i++) {
+    			li = document.createElement("li");
+    			li.innerHTML = '<span class="serverListName">' + vlc.serverList[i].serverName + '</span>' + 
+    						   '<span class="serverListIP">' + vlc.serverList[i].serverIP + '</span>';
+    			li.addEventListener("click",app.initVLCconnection(i));
+    			list.appendChild(li);
+    		}
+    		li = document.createElement("li");
+			li.innerHTML = 'Add new server';
+			li.addEventListener("click",function(){app.show("server");});
+			list.appendChild(li);
+    		//app.snapList = tau.helper.SnapListStyle.create(list);
+    		//app.snapList.destroy(); //The handlers don't work here, so disable them.
+    	} else if (view==="message") {
+    		app.hideAllViews();
+    		app.showView("viewMessage");	
+    		var lastView=app.view;
+    		app.back = function(ev) {
+    			app.show(lastView); 
+    			if (ev) {
+    				ev.stopPropagation();
+    			}
+    		};
+    		app.view=view;
     	}
+    },
+    addVLCconnection: function() {
+    	var server = {};
+    	server.serverIP = document.getElementById("inputIP").value;
+    	server.password = document.getElementById("inputPassword").value;
+    	server.serverName = "new server";
+    	server.serverPort = "8080";
+    	
+    	vlc.serverList[vlc.serverList.length] = server;
+    	
+    	localStorage.setItem("serverList",JSON.stringify(vlc.serverList));
+    	
+    	vlc.connect(vlc.serverList.length - 1);
+    },
+    initVLCconnection: function(id) {
+    	return function () {
+    		var index=id;
+    		vlc.connect(index);
+    	};
     }
 };
 
--- js/vlc.js
+++ js/vlc.js
@@ -1,14 +1,22 @@
-var app //from app.js
+var app; //from app.js
 
 var vlc = {
+	serverList: [],
 	password: "",
 	serverIP: "",
 	serverPort: "8080",
-	timeoutForHTTPRequest: 12000,
 	serverName: "VLCTest",
-
-	makeRequest: function (request) {
+	timeoutForHTTPRequest: 12000,
+	
+	init: function() {
+		console.log("reading serverList");
+		vlc.serverList = JSON.parse(localStorage.getItem("serverList")) || [];
+		console.log("serverList: "+vlc.serverList.length);
+	},
+	makeRequest: function (request, handlerSuccess, handlerErrorCode, handlerTimeout, handlerConnError) {
 		request = request || '';
+		handlerSuccess= handlerSuccess || function() {};
+		
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', 'http://' + vlc.serverIP + ':' + vlc.serverPort + '/requests/status.json?' + request, true);
 			//xhr.setRequestHeader('Authorization', 'Basic ' + encode64(serverUser + ':' + serverPass));
@@ -33,6 +41,7 @@ var vlc = {
 						vlc.seek   = res.time || 0;
 						vlc.seek   = (vlc.seek / vlc.length) * 100;
 						vlc.seek   = Math.round(vlc.seek);
+						handlerSuccess(xhr);
 						app.updateView();
 					} else {
 						console.log('Invalid response received! ' + JSON.stringify(xhr));
@@ -40,21 +49,68 @@ var vlc = {
 					}
 				} else {
 					console.log('Request returned error code ' + xhr.status.toString());
+					handlerErrorCode(xhr);
 					//sendAppMessage('Error: ' + xhr.statusText);
 				}
 			}
 		};
-		xhr.ontimeout = function() {
-			console.log('HTTP request timed out');
-			//sendAppMessage('Error: Request timed out!');
-		};
-		xhr.onerror = function() {
-			console.log('HTTP request return error');
-			//sendAppMessage('Error: Failed to connect!');
-		};
+		handlerTimeout = handlerTimeout || function() {
+				console.log('HTTP request timed out');
+				//sendAppMessage('Error: Request timed out!');
+			}; 
+		handlerConnError = handlerConnError || function() {
+				console.log('HTTP request return error');
+				//sendAppMessage('Error: Failed to connect!');
+			}; 
+		xhr.ontimeout = handlerTimeout; 
+		xhr.onerror = handlerConnError; 
 		xhr.send(null);
 	},
-	getStatus: function() {
-		vlc.makeRequest();
+	connect: function(index) {
+		vlc.serverIP   = vlc.serverList[index].serverIP;
+		vlc.serverPort = vlc.serverList[index].serverPort;
+		vlc.serverName = vlc.serverList[index].serverName;
+		vlc.password   = vlc.serverList[index].password;
+		
+		vlc.makeRequest("", 
+			function() {
+				app.show("remote");
+				if (vlc.watcher) {
+					window.clearInterval(vlc.watcher);
+					vlc.watcher=null;
+				}
+	    		vlc.watcher=window.setInterval(vlc.updateStatus, 1000);
+			}, 
+			function(xhr) {
+				app.showMessage("Could not connect to server: Error code " + xhr.status.toString());
+			},
+			function() {
+				app.showMessage("No response from server " + vlc.serverIP);
+			},
+			function() {
+				app.showMessage("Failed to connect");
+			}
+		);
+	},
+	disconnect: function() {
+		if (vlc.watcher) {
+			window.clearInterval(vlc.watcher);
+			vlc.watcher=null;
+		}
+	},
+	onDisconnect: function() {
+		vlc.disconnect();
+		if (app.view === "remote") {
+			app.back();
+		}
+		app.showMessage("Server disconnected");
+	},
+	updateStatus: function() {
+		vlc.makeRequest("",
+					null, //onSuccess
+					vlc.onDisconnect,  //onErrorCode
+					vlc.onDisconnect,  //onTimeout
+					vlc.onDisconnect   //onConnError
+				);
 	}
-}
+};
